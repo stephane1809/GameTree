3c8f11858a838a685958e0f71ee54e5c8251fb7b
Milena Maia <milenamaiaaraujo@gmail.com>
2023-04-25 09:21:15 -0300

    :style: create onboard model

--- GameTree/GameTree/ContentView.swift
+++ GameTree/GameTree/ContentView.swift
@@ -8,77 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
-
-    var tapped: String = ""
-    @StateObject var gameModel = GameModel.shared
-    @State var isSelected: Bool = true
-
-    var selected = "speaker.wave.3.fill"
-    var notSelected = "speaker.slash.fill"
     var body: some View {
-        NavigationView {
-            VStack(alignment: .center, spacing: 20) {
-                Text("RECORD")
-                    .scaledFont(name: "Georgia", size: 34)
-                recordsView
-                HStack(alignment: .center, spacing: 40) {
-                    navigation
+        Onboard()
+    }
+}
+
+struct MainView: View {
+
+        var tapped: String = ""
+        @StateObject var gameModel = GameModel.shared
+        @State var isSelected: Bool = true
+
+        var selected = "speaker.wave.3.fill"
+        var notSelected = "speaker.slash.fill"
+        var body: some View {
+            NavigationView {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("RECORD")
+                        .scaledFont(name: "Georgia", size: 34)
+                    recordsView
+                    HStack(alignment: .center, spacing: 40) {
+                        navigation
+                        }
+                    }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        buttonMusic
                     }
                 }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    buttonMusic
+            }
+        }
+
+        var buttonMusic: some View {
+            Button {
+                withAnimation {
+                    isSelected.toggle()
                 }
+            } label: {
+                Image(systemName: isSelected ? selected : notSelected)
+                    .foregroundColor(.black)
+                    .scaledFont(name: "Georgia", size: 17)
             }
         }
-    }
-
-    var buttonMusic: some View {
-        Button {
-            withAnimation {
-                isSelected.toggle()
-            }
-        } label: {
-            Image(systemName: isSelected ? selected : notSelected)
-                .foregroundColor(.black)
-                .scaledFont(name: "Georgia", size: 17)
-        }
-    }
-    @ViewBuilder
-    var recordsView: some View {
-            HStack {
-                Text("\(gameModel.record)")
-                    .scaledFont(name: "Georgia", size: 34)
-                Image(systemName: "tree")
-
-                    .foregroundColor(.green)
-                    .scaledFont(name: "Georgia", size: 34)
-            }
-    }
-
-    var navigation: some View {
-
-        NavigationLink(
-            destination: GameView().navigationBarBackButtonHidden(),
-            label: {
+        @ViewBuilder
+        var recordsView: some View {
                 HStack {
-                    Text("Play")
-                        .foregroundColor(.black)
+                    Text("\(gameModel.record)")
                         .scaledFont(name: "Georgia", size: 34)
-                    Image(systemName: "play.circle")
-                        .scaledFont(name: "Georgia", size: 34)
-                        .foregroundColor(.black)
-                }
-                .frame(width: 150, height: 50)
-                .overlay(RoundedRectangle(cornerRadius: 10)
-                    .stroke(.black, lineWidth: 2))
-            }
-        )
-    }
-}
+                    Image(systemName: "tree")
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+                        .foregroundColor(.green)
+                        .scaledFont(name: "Georgia", size: 34)
+                }
+        }
+
+        var navigation: some View {
+
+            NavigationLink(
+                destination: GameView().navigationBarBackButtonHidden(),
+                label: {
+                    HStack {
+                        Text("Play")
+                            .foregroundColor(.black)
+                            .scaledFont(name: "Georgia", size: 34)
+                        Image(systemName: "play.circle")
+                            .scaledFont(name: "Georgia", size: 34)
+                            .foregroundColor(.black)
+                    }
+                    .frame(width: 150, height: 50)
+                    .overlay(RoundedRectangle(cornerRadius: 10)
+                        .stroke(.black, lineWidth: 2))
+                }
+            )
+        }
     }
-}
+
---
+++ GameTree/GameTree/Onboard.swift
@@ -0,0 +1,60 @@
+//
+//  Onboard.swift
+//  GameTree
+//
+//  Created by Milena Maia Araújo on 24/04/23.
+//
+
+import SwiftUI
+import Lottie
+
+struct Onboard: View {
+    var onBoardingModel: [Onboarding] = [
+        Onboarding(lottie: "leaf", desc: "Hi, small leaf", button: "", height: UIScreen.main.bounds.size.height * 0.4),
+        Onboarding(lottie: "tree", desc: "Touch the trees to save then from the fire", button: "", height: UIScreen.main.bounds.size.height * 0.4),
+        Onboarding(lottie: "fire", desc: "Ready?", button: "Let's go", height: UIScreen.main.bounds.size.height * 0.2)
+    ]
+   @State private var index = 0
+    var body: some View {
+        NavigationStack {
+            TabView(selection: $index) {
+                ForEach(0..<onBoardingModel.count, id: \.self) {model in
+                    VStack(alignment: .center) {
+                        LottieView(lottieFile: onBoardingModel[model].lottie)
+                            .frame(width: UIScreen.main.bounds.size.width, height: onBoardingModel[model].height, alignment: .center)
+
+                        Text(onBoardingModel[index].desc)
+                        if onBoardingModel[index].button != "" {
+                            NavigationLink {
+                                MainView()
+                                    .navigationBarBackButtonHidden(true)
+                                    .indexViewStyle(.page(backgroundDisplayMode: .never))
+                            } label: {
+                                Text("Let's go!")
+                                    .foregroundColor(Color.white)
+                                    .font(.title3)
+                                    .padding()
+                                    .cornerRadius(12)
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 10)
+                                    )
+                            }
+                        }
+                    }
+                }
+
+            }.tabViewStyle(.page(indexDisplayMode: .automatic))
+                .indexViewStyle(.page(backgroundDisplayMode: .always))
+                .padding()
+        }
+    }
+}
+
+struct Onboarding: Hashable, Equatable {
+    var lottie: String
+    var desc: String
+    var button: String
+    var height: Double
+}
+
+
