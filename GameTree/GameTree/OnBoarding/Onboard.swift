c2e2e04cf362dab611778b21807db4fcb5dddd84
Milena Maia <milenamaiaaraujo@gmail.com>
2023-04-26 10:37:42 -0300

    :lipstick: style: changing onboard

--- GameTree/GameTree/OnBoarding/Onboard.swift
+++ GameTree/GameTree/OnBoarding/Onboard.swift
@@ -13,7 +13,7 @@ struct Onboard: View {
         Onboarding(lottie: "leaf", desc: "Hi, small leaf", button: "", height: UIScreen.main.bounds.size.height * 0.4),
         Onboarding(lottie: "tree", desc: "Touch the trees to save then from the fire",
                    button: "", height: UIScreen.main.bounds.size.height * 0.4),
-        Onboarding(lottie: "firee", desc: "Ready?", button: "Let's go", height: UIScreen.main.bounds.size.height * 0.2)
+        Onboarding(lottie: "fire", desc: "Are you ready?", button: "Let's go", height: UIScreen.main.bounds.size.height * 0.2)
     ]
    @State private var index = 0
     var body: some View {
@@ -24,15 +24,17 @@ struct Onboard: View {
                         LottieView(lottieFile: onBoardingModel[model].lottie)
                             .frame(width: UIScreen.main.bounds.size.width,
                                    height: onBoardingModel[model].height, alignment: .center)
-
+//                            .padding(.top, 100)
+                        
                         Text(onBoardingModel[index].desc)
+
                         if onBoardingModel[index].button != "" {
                             NavigationLink {
                                 MainView()
                                     .navigationBarBackButtonHidden(true)
                                     .indexViewStyle(.page(backgroundDisplayMode: .never))
                             } label: {
-                                Text("Let's go!")
+                                Text(onBoardingModel[index].button)
                                     .foregroundColor(Color.white)
                                     .font(.title3)
                                     .padding()
@@ -40,6 +42,7 @@ struct Onboard: View {
                                     .background(
                                         RoundedRectangle(cornerRadius: 10)
                                     )
+//                                    .frame(width: UIScreen.main.bounds.size.height * 0.2, height: UIScreen.main.bounds.size.height * 0.2)
                             }
                         }
                     }
@@ -49,5 +52,14 @@ struct Onboard: View {
                 .indexViewStyle(.page(backgroundDisplayMode: .always))
                 .padding()
         }
+    }
+}
+
+struct Onboard_Previews: PreviewProvider {
+
+    @State static var lost: Bool = false
+
+    static var previews: some View {
+        Onboard()
     }
 }
